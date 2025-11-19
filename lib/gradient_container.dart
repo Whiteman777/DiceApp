46795143c7f79a8b2af0de9040254ce09de68fa7
Youssef Waleed Mohamed Ahmed <youssefwalid725@gmail.com>
2025-11-19 14:34:31 +0200

    intial commit

---
+++ lib/gradient_container.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:diceapp/styled_text.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.deepPurple, Colors.blueGrey],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: StyledText(),
+      ),
+    );
+  }
+}
